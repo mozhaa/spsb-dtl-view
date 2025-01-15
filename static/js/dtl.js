fc710dbabfb3231a2017b3f73130c9febc8a5501
mozhaa <mozhay2005@gmail.com>
2025-01-15 15:52:33 +0300

    videos in tierlist

---
+++ static/js/dtl.js
@@ -0,0 +1,12 @@
+$(() => {
+    $(".embed-video-placeholder").on("click", function () {
+        let embed = $(this).data("src-embed");
+        let direct = $(this).data("src-direct");
+        let video =
+            embed.length > 0
+                ? `<iframe class="embed-video" src="${embed}" frameborder="0" allowfullscreen></iframe>`
+                : `<div class="embed-video"><p>Failed to embed video. <a href="${direct}">Direct link</a></p></div>`;
+        $(this).parent().append(video);
+        $(this).remove();
+    });
+});
