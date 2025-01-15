23899d3632f2cefb024c1b5a8c844f57cb43fd5a
mozhaa <mozhay2005@gmail.com>
2025-01-15 17:44:54 +0300

    bug fix: get full sass path

--- src/app/scss.py
+++ src/app/scss.py
@@ -1,10 +1,11 @@
 import logging
 import subprocess
+import shutil
 
 logger = logging.getLogger(__name__)
 
 
 def compile_scss() -> None:
-    ret_code = subprocess.call(["sass", "scss:static/css", "--style", "compressed"], shell=True)
+    ret_code = subprocess.call([shutil.which("sass"), "scss:static/css", "--style", "compressed"])
     if ret_code != 0:
         logger.error(f"SCSS compilation finished with non-zero return code ({ret_code})")
